d10f4840e5b4c49595fc30ca5f3edec5676617e7
rounak-meyur <rm5nz@virginia.edu>
2023-02-14 12:18:58 -0800

    updated cvxopt setup

--- pyEVCSlib.py
+++ pyEVCSlib.py
@@ -753,72 +753,77 @@ class EVCSFixture(unittest.TestCase):
 
 
 
-fx = EVCSFixture('runTest')
-fx.out_dir = "out/script"
-fx.fig_dir = "figs/script"
-fx.grb_dir = "gurobi/script"
-fx.area = 'Area 2'
+# fx = EVCSFixture('runTest')
+# fx.out_dir = "out/script"
+# fx.fig_dir = "figs/script"
+# fx.grb_dir = "gurobi/script"
+# fx.area = 'Area 2'
+
+# fx.plot_dependence(csv_file="demand_lamb_1000000.csv", 
+#                    suptitle_sfx = "additional length for routing power lines")
+
+# import sys
+# sys.exit(0)
 
 
+# volt_range = [0.95, 0.92, 0.90]
+# data = {"rating":[], "connection":[], "length":[]}
+# data.update(
+#     {f"less than {v}":[] for v in volt_range}
+#     )
+# lambda_ = 1000000
+# rating_list = [30, 50, 100, 120, 150]
 
-volt_range = [0.95, 0.92, 0.90]
-data = {"rating":[], "connection":[], "length":[]}
-data.update(
-    {f"less than {v}":[] for v in volt_range}
-    )
-lambda_ = 1000000
-rating_list = [1800, 2000, 2400, 3000, 3600, 4800]
+# for conn_type in ["optimal", "nearest"]:
 
-for conn_type in ["optimal", "nearest"]:
-
-    for k in tqdm(range(len(rating_list)), 
-                  desc="Computing for different EV charger ratings",
-                  ncols=100):
-        fx.demand = float(rating_list[k] / 24.0)
+#     for k in tqdm(range(len(rating_list)), 
+#                   desc="Computing for different EV charger ratings",
+#                   ncols=100):
+#         fx.demand = float(rating_list[k] / 24.0)
         
-        # initial read
-        synth_net, evcs = fx.read_inputs()
-        init_length = sum([synth_net.edges[e]["length"] \
-                            for e in synth_net.edges])
+#         # initial read
+#         synth_net, evcs = fx.read_inputs()
+#         init_length = sum([synth_net.edges[e]["length"] \
+#                             for e in synth_net.edges])
         
-        # additional edges for routing
-        synth_net = fx.connect_evcs(
-            synth_net, evcs, 
-            connection=conn_type,
-            lambda_ = lambda_, 
-            epsilon=1e-1,)
-        final_length = sum([synth_net.edges[e]["length"] \
-                            for e in synth_net.edges])
+#         # additional edges for routing
+#         synth_net = fx.connect_evcs(
+#             synth_net, evcs, 
+#             connection=conn_type,
+#             lambda_ = lambda_, 
+#             epsilon=1e-1,)
+#         final_length = sum([synth_net.edges[e]["length"] \
+#                             for e in synth_net.edges])
         
-        # Evaluate the additional length
-        add_length = final_length - init_length
+#         # Evaluate the additional length
+#         add_length = final_length - init_length
         
-        # Add it to the data
-        data["connection"].append(conn_type)
-        data["rating"].append(rating_list[k])
-        data["length"].append(add_length)
+#         # Add it to the data
+#         data["connection"].append(conn_type)
+#         data["rating"].append(rating_list[k])
+#         data["length"].append(add_length)
         
-        # run powerflow and number of nodes outside limit
-        powerflow(synth_net)
-        nodelist = [n for n in synth_net if synth_net.nodes[n]['label']!='R']
-        for v in volt_range:
-            num_nodes = len([n for n in nodelist if synth_net.nodes[n]["voltage"] < v])
-            data[f"less than {v}"].append(num_nodes)
+#         # run powerflow and number of nodes outside limit
+#         powerflow(synth_net)
+#         nodelist = [n for n in synth_net if synth_net.nodes[n]['label']!='R']
+#         for v in volt_range:
+#             num_nodes = len([n for n in nodelist if synth_net.nodes[n]["voltage"] < v])
+#             data[f"less than {v}"].append(num_nodes)
 
-# Create the dataframe
-df = pd.DataFrame(data)
-df.to_csv(f"{fx.out_dir}/demand_lamb_{lambda_}.csv", index=False)
+# # Create the dataframe
+# df = pd.DataFrame(data)
+# df.to_csv(f"{fx.out_dir}/demand_lamb_{lambda_}.csv", index=False)
 
-# Plot the dependence
-fig, ax = fx.plot_dependence(
-    df_data=df,
-    suptitle_sfx = f"Additional network length versus demand : lambda = {lambda_}",
-    file_name_sfx = f"demand_dependence_lamb_{lambda_}", 
-    fontsize=20,
-    do_return=True
-)
-fx.assertIsNotNone(fig)
-fx.assertIsNotNone(ax)
+# # Plot the dependence
+# fig, ax = fx.plot_dependence(
+#     df_data=df,
+#     suptitle_sfx = f"Additional network length versus demand : lambda = {lambda_}",
+#     file_name_sfx = f"demand_dependence_lamb_{lambda_}", 
+#     fontsize=20,
+#     do_return=True
+# )
+# fx.assertIsNotNone(fig)
+# fx.assertIsNotNone(ax)
 
 
 
