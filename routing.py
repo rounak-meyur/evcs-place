55868d08dc6895171d94377aac349f036941b44c
Rounak Meyur <45717264+rounak-meyur@users.noreply.github.com>
2023-02-21 08:49:03 -0800

    updated code

--- routing.py
+++ routing.py
@@ -1,3 +1,4 @@
+import os, sys
 from pyEVCSlib import EVCSFixture
 from pyUtilslib import powerflow
 import pandas as pd
@@ -25,58 +26,104 @@ rating_list = [30, 50, 100, 120, 150, 180, 250, 350]
 #              'Area 15', 'Area 16', 'Area 17', 'Area 18', 'Area 19', 'Area 20']
         
 area_list = ['Area 1', 'Area 4', 'Area 8', 'Area 11', 'Area 12', 'Area 15']
+area_list = ['Area 6']
 
-
-for area in area_list[11:]:
+for area in area_list:
     fx.area = area
-
-    data = {"lambda":[], "rating":[], "connection":[], "length":[]}
-    data.update({f"< {v}":[] for v in volt_range})
-    for conn_type in ["nearest", "optimal"]:
+    # check if csv file for the area exists
+    filename = f"{fx.out_dir}/{fx.area}_summary.csv"
+    if os.path.exists(filename):
+        df = pd.read_csv(filename, header=0)
+        data = df.to_dict()
+        sys.exit(0)
+    else:
+        data = {"lambda":[], "rating":[], "connection":[], "length":[]}
+        data.update({f"< {v}":[] for v in volt_range})
     
-        for lambda_ in lambda_list:
-            for k in range(len(rating_list)):
-                
-                print(f"Computing for {fx.area} : connection={conn_type} : lambda={lambda_} : rating={rating_list[k]}")
-                fx.demand = float(rating_list[k] * 1000 / 24.0)
-                
-                data["lambda"].append(lambda_)
-                data["connection"].append(conn_type)
-                data["rating"].append(rating_list[k])
-                
-                # initial read
-                synth_net, evcs = fx.read_inputs()
-                init_length = sum([synth_net.edges[e]["length"] \
+    # nearest available point algorithm
+    for rating in rating_list:
+        fx.demand = float(rating * 1000 / 24.0)
+        data["lambda"].append(1)
+        data["connection"].append("nearest")
+        data["rating"].append(rating)
+        
+        # initial read
+        synth_net, evcs = fx.read_inputs()
+        init_length = sum([synth_net.edges[e]["length"] \
+                            for e in synth_net.edges])
+        
+        # additional edges for nearest point routing
+        synth_net,opt_flag = fx.connect_evcs(
+            synth_net, evcs, 
+            connection="nearest",
+            epsilon=1e-1,)
+        
+        if opt_flag:
+            final_length = sum([synth_net.edges[e]["length"] \
+                                for e in synth_net.edges])
+            
+            # Evaluate the additional length
+            add_length = final_length - init_length
+        
+            # Add it to the data
+            data["length"].append(add_length)
+        
+            # run powerflow and number of nodes outside limit
+            powerflow(synth_net)
+            nodelist = [n for n in synth_net if synth_net.nodes[n]['label']!='R']
+            for v in volt_range:
+                num_nodes = len([n for n in nodelist if synth_net.nodes[n]["voltage"] < v])
+                data[f"< {v}"].append(num_nodes * 100.0 / len(nodelist))
+        else:
+            data["length"].append(float("nan"))
+            for v in volt_range:
+                data[f"< {v}"].append(float("nan"))
+    
+    
+    # optimal routing algorithm
+    for lambda_ in lambda_list:
+        for rating in rating_list:
+            
+            print(f"Computing for {fx.area} : lambda={lambda_} : rating={rating}")
+            fx.demand = float(rating * 1000 / 24.0)
+            
+            data["lambda"].append(lambda_)
+            data["connection"].append("optimal")
+            data["rating"].append(rating)
+            
+            # initial read
+            synth_net, evcs = fx.read_inputs()
+            init_length = sum([synth_net.edges[e]["length"] \
+                                for e in synth_net.edges])
+            
+            # additional edges for routing
+            synth_net,opt_flag = fx.connect_evcs(
+                synth_net, evcs, 
+                connection="optimal",
+                lambda_ = lambda_, 
+                epsilon=1e-1,)
+            
+            if opt_flag:
+                final_length = sum([synth_net.edges[e]["length"] \
                                     for e in synth_net.edges])
                 
-                # additional edges for routing
-                synth_net,opt_flag = fx.connect_evcs(
-                    synth_net, evcs, 
-                    connection=conn_type,
-                    lambda_ = lambda_, 
-                    epsilon=1e-1,)
-                
-                if opt_flag:
-                    final_length = sum([synth_net.edges[e]["length"] \
-                                        for e in synth_net.edges])
-                    
-                    # Evaluate the additional length
-                    add_length = final_length - init_length
-                
-                    # Add it to the data
-                    data["length"].append(add_length)
-                
-                    # run powerflow and number of nodes outside limit
-                    powerflow(synth_net)
-                    nodelist = [n for n in synth_net if synth_net.nodes[n]['label']!='R']
-                    for v in volt_range:
-                        num_nodes = len([n for n in nodelist if synth_net.nodes[n]["voltage"] < v])
-                        data[f"< {v}"].append(num_nodes * 100.0 / len(nodelist))
-                else:
-                    data["length"].append(float("nan"))
-                    for v in volt_range:
-                        data[f"< {v}"].append(float("nan"))
+                # Evaluate the additional length
+                add_length = final_length - init_length
+            
+                # Add it to the data
+                data["length"].append(add_length)
+            
+                # run powerflow and number of nodes outside limit
+                powerflow(synth_net)
+                nodelist = [n for n in synth_net if synth_net.nodes[n]['label']!='R']
+                for v in volt_range:
+                    num_nodes = len([n for n in nodelist if synth_net.nodes[n]["voltage"] < v])
+                    data[f"< {v}"].append(num_nodes * 100.0 / len(nodelist))
+            else:
+                data["length"].append(float("nan"))
+                for v in volt_range:
+                    data[f"< {v}"].append(float("nan"))
     
     # Create the dataframe
     df = pd.DataFrame(data)
-    df.to_csv(f"{fx.out_dir}/{fx.area}_summary.csv", index=False)
+    df.to_csv(filename, index=False)
